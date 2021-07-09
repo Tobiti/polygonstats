251fc6ce173d338edce1b6af57535765e15d6b62
Tobias Adloff <tadloff@mail.upb.de>
2021-07-09 20:41:10 +0200

    Fix fort query

--- PolygonStats/RocketMap/RocketMapManager.cs
+++ PolygonStats/RocketMap/RocketMapManager.cs
@@ -89,7 +89,7 @@ namespace PolygonStats.RocketMap
                         String query =  "INSERT INTO pokestop (pokestop_id, enabled, latitude, longitude, last_modified, lure_expiration, " +
                                         "last_updated, image, active_fort_modifier, incident_start, incident_expiration, incident_grunt_type, " +
                                         "is_ar_scan_eligible) " +
-                                        "VALUES (\"{0}\", {1}, {2}, {3}, \"{4}\", \"{5}\", \"{6}\", \"{7}\", {8}, \"{9}\", \"{10}\", {11}, {12}) " +
+                                        "VALUES (\"{0}\", {1}, {2}, {3}, \"{4}\", \"{5}\", \"{6}\", \"{7}\", {8}, {9}\", \"{10}\", {11}, {12}) " +
                                         "ON DUPLICATE KEY UPDATE last_updated=VALUES(last_updated), lure_expiration=VALUES(lure_expiration), " +
                                         "last_modified=VALUES(last_modified), latitude=VALUES(latitude), longitude=VALUES(longitude), " +
                                         "active_fort_modifier=VALUES(active_fort_modifier), incident_start=VALUES(incident_start), " +
@@ -100,8 +100,8 @@ namespace PolygonStats.RocketMap
                             query = String.Format(query, fort.FortId, fort.Enabled, fort.Latitude, fort.Longitude, ToMySQLDateTime(UnixTimeStampToDateTime(fort.LastModifiedMs)), 
                                                         ToMySQLDateTime(UnixTimeStampToDateTime(fort.LastModifiedMs).AddMinutes(30)), 
                                                         ToMySQLDateTime(DateTime.UtcNow), fort.ImageUrl, (fort.ActiveFortModifier.Count > 0 ? (int)fort.ActiveFortModifier[0] : 0),
-                                                        (fort.PokestopDisplays.Count <= 0 ? "NULL" : ToMySQLDateTime(UnixTimeStampToDateTime(fort.PokestopDisplays[0].IncidentStartMs))),
-                                                        (fort.PokestopDisplays.Count <= 0 ? "NULL" : ToMySQLDateTime(UnixTimeStampToDateTime(fort.PokestopDisplays[0].IncidentExpirationMs))),
+                                                        (fort.PokestopDisplays.Count <= 0 ? "NULL" : $"\"{ToMySQLDateTime(UnixTimeStampToDateTime(fort.PokestopDisplays[0].IncidentStartMs))}\""),
+                                                        (fort.PokestopDisplays.Count <= 0 ? "NULL" : $"\"{ToMySQLDateTime(UnixTimeStampToDateTime(fort.PokestopDisplays[0].IncidentExpirationMs))}\""),
                                                         (fort.PokestopDisplays.Count <= 0 ? "NULL" : (int)fort.PokestopDisplays[0].CharacterDisplay.Character),
                                                         fort.IsArScanEligible);
 
