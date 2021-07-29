267e39d5b18629f1fddeca6387280a6c15b543fe
Tobias Adloff <tadloff@mail.upb.de>
2021-07-29 20:00:43 +0200

    Remove async

--- PolygonStats/ClientSession.cs
+++ PolygonStats/ClientSession.cs
@@ -457,7 +457,7 @@ namespace PolygonStats
                             context.Database.ExecuteSqlRaw($"UPDATE `SessionLogEntry` SET PokemonName=\"{pokemon.PokemonId.ToString("G")}\", Attack={pokemon.IndividualAttack}, Defense={pokemon.IndividualDefense}, Stamina={pokemon.IndividualStamina} WHERE PokemonUniqueId={pokemon.Id} ORDER BY Id");
                             if (pokemon.IndividualAttack == 15 && pokemon.IndividualDefense == 15 && pokemon.IndividualStamina == 15)
                             {
-                                context.Database.ExecuteSqlRawAsync($"UPDATE `Session` SET MaxIV=MaxIV+1, LastUpdate=NOW() WHERE Id={dbSessionId} ORDER BY Id");
+                                context.Database.ExecuteSqlRaw($"UPDATE `Session` SET MaxIV=MaxIV+1, LastUpdate=NOW() WHERE Id={dbSessionId} ORDER BY Id");
                             }
                         }
                     }
