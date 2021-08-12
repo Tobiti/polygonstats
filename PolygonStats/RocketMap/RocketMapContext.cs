9f0048dec9fcb40e359ee05c5176a7d4938b0a85
Tobias Adloff <tadloff@mail.upb.de>
2021-08-12 12:45:42 +0200

    Fix compile error

--- PolygonStats/RocketMap/RocketMapContext.cs
+++ PolygonStats/RocketMap/RocketMapContext.cs
@@ -2,6 +2,8 @@
 using PolygonStats.Configuration;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
